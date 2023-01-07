2676eecec2bcea39a82892268de0f672e3fce9a3
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-01-07 13:00:45 -0300

    Implementação do carregamento de novas imagens

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,61 @@
-async function mapImageList(){
-    const memesObject = [
-        {
-            name: "chapolin",
-            path: "memeficator/src/img/chapolin.jpg"
-        },
-        {
-            name: "chloe",
-            path: "memeficator/src/img/chloe(1).jpg"
-        },
-        {
-            name: "funny-cat1",
-            path: "memeficator/src/img/funny-cat1.jpg"
-        },
-        {
-            name: "funny-cat2",
-            path: "memeficator/src/img/funny-cat2.jpg"
-        }
-    ]
-    return memesObject;
+
+
+function enablePhotoUpload() {
+  const imageInput = document.getElementById("image-input");
+  imageInput.addEventListener("change", function() {
+   
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+   
+      const uploadImage = reader.result;
+        changeMemePicture(uploadImage);
+  })
+    reader.readAsDataURL(this.files[0])
+});
 }
 
-async function createGallery (imageList){
-    const memeSelector = document.getElementById("memes-list")
-    imageList.forEach(image => {
-        let newOption = document.createElement("option");
-        newOption.text = image.name;
-        newOption.value = image.path;
-        memeSelector.appendChild(newOption);
-        
-    });
+async function mapImageList() {
+  const memesObject = [
+    {
+      name: "chapolin",
+      path: "src/img/chapolin.jpg",
+    },
+    {
+      name: "chloe",
+      path: "src/img/chloe.jpg",
+    },
+    {
+      name: "funny-cat1",
+      path: "src/img/funny-cat1.jpg",
+    },
+    {
+      name: "funny-cat2",
+      path: "src/img/funny-cat2.jpg",
+    },
+  ];
+  return memesObject;
+}
+
+async function createGallery(imageList) {
+  const memeSelector = document.querySelector("#memes-list");
+  imageList.forEach(image => {
+    let newOption = document.createElement("option");
+    newOption.text = image.name;
+    newOption.value = image.path;
+    memeSelector.appendChild(newOption);
+  });
+}
+
+async function changeMemePicture(photo) {
+  let displayImage = document.querySelector("#display-image");
+  displayImage.style.backgroundImage = `url('${photo}')`;
 }
 
 async function main() {
-    const memesImageList = await mapImageList();   
-    await createGallery(memesImageList); 
+  const memesImageList = await mapImageList();
+  enablePhotoUpload();
+  await createGallery(memesImageList);
+  await changeMemePicture(memesImageList[0].path);
 }
 
 main();
-
